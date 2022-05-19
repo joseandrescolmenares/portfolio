d75dec089b9a3819acee5bb5ff2a86caba809d9e
jose <joseandrescolmenares02@gmail.com>
2022-05-19 15:05:10 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Animacion from './components/Animacion.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    
+     <Animacion/>
+    </div>
+  );
+}
+
+export default App;
