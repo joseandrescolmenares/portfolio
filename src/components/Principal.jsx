17fcb473d7e6fb3ed9151077fbe29e0bf2eb15ba
jose <joseandrescolmenares02@gmail.com>
2022-07-01 14:41:15 -0300

    terminado

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -17,7 +17,7 @@ const Principal = () => {
       {
         
         position: "bottom-left",
-        autoClose: 5000,
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
